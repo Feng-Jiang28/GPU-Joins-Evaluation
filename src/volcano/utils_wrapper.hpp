626c5b82e8968e943f11dccc94a88aa1103a84d7
fejiang <fejiang@nvidia.comm>
2024-08-04 13:45:34 +0800

    change
    Signed-off-by: fejiang <fejiang@nvidia.comm>

--- src/volcano/utils_wrapper.hpp
+++ src/volcano/utils_wrapper.hpp
@@ -1,9 +1,13 @@
 #ifndef UTILS_WRAPPER_H
 #define UTILS_WRAPPER_H
 #include "utils.cuh"
-
+#include <iostream>
+#include "mem_manager.hpp"
+#include <cuda.h>
 // Declare functions you want to use in .cpp files
 
-
+// Forward declaration of the release_mem template function
+template<typename T>
+void release_mem(T* ptr, cudaStream_t stream = 0);
 
 #endif
