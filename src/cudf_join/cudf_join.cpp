ef3ed58e2353ad5f9abbcc1c511ae0f99960973b
fejiang <fejiang@nvidia.comm>
2024-08-04 19:01:06 +0800

    change
    Signed-off-by: fejiang <fejiang@nvidia.comm>

--- src/cudf_join/cudf_join.cpp
+++ src/cudf_join/cudf_join.cpp
@@ -190,6 +190,6 @@ void run_rest_multicols(const struct join_args& args) {
 
 int main(int argc, char** argv){
     //cout << "hello world! \n";
-    prepare_running(int argc, char** argv);
+    prepare_running(argc, argv);
     return 0;
 }
