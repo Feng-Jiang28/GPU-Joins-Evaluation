f5e0c3eeade717d16c55afcbd8bd8eca5f5e369f
fejiang <fejiang@nvidia.comm>
2024-08-05 13:04:56 +0800

    change
    Signed-off-by: fejiang <fejiang@nvidia.comm>

--- src/cudf_join_test/cudf_join_test.hpp
+++ src/cudf_join_test/cudf_join_test.hpp
@@ -1,7 +1,16 @@
 #pragma once
+
+#include <cudf/io/csv.hpp>
+#include <cudf/io/datasource.hpp>
+
+#include <rmm/cuda_device.hpp>
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+#include <rmm/mr/device/device_memory_resource.hpp>
+#include <rmm/mr/device/owning_wrapper.hpp>
+#include <rmm/mr/device/pool_memory_resource.hpp>
+
 #include <iostream>
 #include <string>
-#include <cudf/column/column.hpp>
 
 using namespace std;
 // cudf hash inner join function
@@ -15,10 +24,21 @@ int main(int argc, char const** argv) {
         return 1;
     }
 
-    auto const table_path_r = argv[1];
+    auto const table_path_r = string(argv[1]);
     auto const table_path_s = string(argv[2]);
 
     cout << table_path_r << " : " << table_path_s << "\n";
+
+    auto resource = create_memory_resource("cuda");
+    rmm::mr::set_current_device_resource(resouce.get());
+
+    auto r_result = [table_path_r]{
+        cudf::io::csv_reader_options in_opts =
+            cudf::io::csv_reader_options::builder(cudf::io::source_info{csv_file}).header(-1);
+        return cudf::io::read_csv(in_opts).tbl;
+    }();
+
+
 
     return 0;
 }
