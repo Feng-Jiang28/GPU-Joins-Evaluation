f39adb128316fc55245e7527168a6223014698d4
fejiang <fejiang@nvidia.comm>
2024-08-05 12:49:18 +0800

    change
    Signed-off-by: fejiang <fejiang@nvidia.comm>

--- src/cudf_join_test/cudf_join_test.hpp
+++ src/cudf_join_test/cudf_join_test.hpp
@@ -1,10 +1,22 @@
 #pragma once
-
 #include <iostream>
 #include <cudf/column/column.hpp>
+
+// cudf hash inner join function
+
+// read from disk function
+
+
 int main(int argc, char const** argv) {
     if (argc < 2){
-        std::cout << "required parameter: file path";
+        std::cout << "required parameter: file path \n";
+        return 1;
     }
+
+    auto const talbe_path_r = std::string(argv[0]);
+    auto const table_path_s = std::string(argv[1]);
+
+    cout << table_path_r << " : " << table_path_s << "\n";
+
     return 0;
 }
