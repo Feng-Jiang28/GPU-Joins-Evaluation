7912197b7ab555405eb1678e74daf5302f1cc511
fejiang <fejiang@nvidia.comm>
2024-08-05 14:10:11 +0800

    change
    Signed-off-by: fejiang <fejiang@nvidia.comm>

--- src/cudf_join_test/cudf_join_test.hpp
+++ src/cudf_join_test/cudf_join_test.hpp
@@ -87,7 +87,7 @@ int main(int argc, char const** argv) {
 
     auto result = inner_join(table_r, table_s, {0, 1}, {0, 1}, cudf::null_equality::EQUAL);
 
-    std::cout << "table_r: " << result-view().num_rows() << " rows " << result->view().num_columns()
+    std::cout << "table_r: " << result->view().num_rows() << " rows " << result->view().num_columns()
          << " columns\n";
 
     return 0;
